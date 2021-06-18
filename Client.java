d224238fd1661808b4c20c6d66a36b6e011ad340
KarinoMachida <83054559+KarinoMachida@users.noreply.github.com>
2021-06-18 14:30:01 +0900

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,9 +1,12 @@
 import java.io.*;
 import java.util.*;
 import java.net.*;
+
 public class Client {
  public static void main(String[] args)throws IOException {
   int port;
+  Boolean judgeflag = true;
+  Janken jk = new Janken();
   if (args.length != 1){
 	port = 8080;
   }else{
@@ -16,13 +19,9 @@ public class Client {
  System.out.println("socket = " + socket);
  BufferedReader in = new BufferedReader( new InputStreamReader( socket.getInputStream()));
  PrintWriter out = new PrintWriter( new BufferedWriter( new OutputStreamWriter( socket.getOutputStream())), true);
- System.out.println("----------------------------------------------------------");
- System.out.println("rock, paper, scissors game");
- System.out.println("play games 5 times ");
- System.out.println("Typing your hand type. rock = 0, paper = 1, scissors = 2");
- System.out.println("----------------------------------------------------------");
+ jk.Rule();
  Scanner scanner = new Scanner(System.in);
- for(int i = 0; i < 5; i++) {
+ while(judgeflag){
  System.out.print("your choice = ");
  int myhand = scanner.nextInt();
  System.out.println("Waiting for the other person to make a choice...");
@@ -30,10 +29,17 @@ public class Client {
  String shand = in.readLine();
  String judge = in.readLine();
  int otherhand = Integer.parseInt(shand);
- System.out.println("My hand is " + myhand + " : other hand is " + otherhand);
+ String handa = jk.Changename(myhand);
+ String handb = jk.Changename(otherhand);
+ System.out.println("My hand is [" + handa + "] : Other hand is [" + handb + "]");
  System.out.println(judge);
  System.out.println("----------------------------------------------------------");
+ int rsltint = Integer.parseInt(in.readLine());
+ if(rsltint != 0){
+	judgeflag = false;
  }
+ }
+ 
  out.println("END");
  } finally {
  System.out.println("closing...");
